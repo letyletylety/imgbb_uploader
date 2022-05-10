c999cfeb3233a383abaa16c089bbf67168d20cd9
Lety <taej0127@gmail.com>
2022-05-10 11:05:33 +0900

    type error fix

--- lib/src/component/image_card.dart
+++ lib/src/component/image_card.dart
@@ -2,7 +2,7 @@ import 'dart:async';
 import 'dart:developer';
 import 'dart:typed_data';
 
-import 'package:cross_file/src/types/interface.dart';
+import 'package:file_selector/file_selector.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
